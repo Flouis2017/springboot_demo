1ee692c06ca93df3d6af36badd77c0743fbd160f
Flouis2017 <315587951@qq.com>
2019-10-24 00:24:50 +0800

    logback configuration

--- src/main/java/com/flouis/demo/controller/UserController.java
+++ src/main/java/com/flouis/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.flouis.demo.controller;
 
 import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,11 +10,13 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
 	@RequestMapping("/hello")
 	@ResponseBody
 	public Map hello(String name){
+		log.info("execute UserController->hello");
 		Map resMap = Maps.newHashMap();
 		resMap.put("msg", "hello " + name + "!");
 		return resMap;
