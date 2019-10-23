fb30dd68b6ed38d1b0ba05712b1fee8b0fd25f7f
Floui2017 <315587951@qq.com>
2019-10-23 14:55:41 +0800

    banner.txt

--- src/main/java/com/flouis/demo/controller/UserController.java
+++ src/main/java/com/flouis/demo/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.flouis.demo.controller;
 
 import com.google.common.collect.Maps;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
-@RestController
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping("/user/hello")
+	@RequestMapping("/hello")
+	@ResponseBody
 	public Map hello(String name){
 		Map resMap = Maps.newHashMap();
 		resMap.put("msg", "hello " + name + "!");
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+	<h2>Hello Springboot!</h2>
+</body>
+</html>
